17012c2732db14a4501a57bc7e1ea79f3170ff29
Victor Fernandes <victorfern91@gmail.com>
2019-08-07 02:08:29 +0100

    Update coding style

--- src/utils/dev.js
+++ src/utils/dev.js
@@ -1,9 +1,11 @@
-export const includeHotReloadCapabilities = Component => {
+export const includeHotReloadCapabilities = component => {
   let hot = null;
 
-  if (process.env.NODE_ENV  === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     hot = require('react-hot-loader').hot;
   }
 
-  return process.env.NODE_ENV  === 'development' ? hot(module)(Component) : Component
+  return process.env.NODE_ENV === 'development' ? hot(module)(component) : component;
 };
+
+export default { includeHotReloadCapabilities };
