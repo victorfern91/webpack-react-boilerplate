017f0fee7f911e065610aab0877940a449270678
Victor Fernandes <victorfern91@gmail.com>
2019-08-13 01:18:13 +0100

    Update Babel config in order to run tests in docker

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,17 @@ module.exports = {
   plugins: ['babel-plugin-syntax-dynamic-import'],
   env: {
     test: {
-      presets: ['@babel/preset-env', '@babel/preset-react']
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current'
+            }
+          }
+        ],
+        '@babel/preset-react'
+      ]
     },
 
     dev: {
